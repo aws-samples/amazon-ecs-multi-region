2b1b4cc14da5b9d3cf45d3d9ca06cc8c9fed2c6a
Ahmed Aboshanab <amshanab@amazon.com>
2022-08-12 12:04:36 +0200

    fix use of secondaryLoadBalancerArn

--- cdk/bin/ecs-multi-region-workshop.ts
+++ cdk/bin/ecs-multi-region-workshop.ts
@@ -50,5 +50,5 @@ const secondaryBackendStack = new EcsBackendStack(app, 'workshop-backend-seconda
 const routingStack = new EcsRoutingStack(app, 'workshop-routing', {
   loadBalancer: mainBackendStack.apiService.loadBalancer,
   env: envMainRegion,
-  secondaryLoadBalancerArn: process.env.WORKSHOP_SECONDARY_ALB_ARN
+  secondaryLoadBalancerArn: process.env.WORKSHOP_SECONDARY_ALB_ARN!
 });
