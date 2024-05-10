8aa2bf89fd09d0ed0fcc37519c8d6f6bda40c167
Hareesh Iyer <iyerh@amazon.com>
2024-05-09 21:23:35 -0400

    updating dependencies

--- cdk/bin/ecs-multi-region-workshop.ts
+++ cdk/bin/ecs-multi-region-workshop.ts
@@ -12,18 +12,26 @@ const dynamodbTableName = 'workshop-table';
 // Environment Settings
 const envMainRegion = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION
+  region: process.env.CDK_DEFAULT_REGION,
+}
+const envSecondaryRegion = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.WORKSHOP_SECONDARY_REGION,
 }
 
 // Foundation Stacks
 const mainFoundationStack = new EcsFoundationStack(app, 'workshop-foundation-main', {
   env: envMainRegion
 });
+const secondaryFoundationStack = new EcsFoundationStack(app, 'workshop-foundation-secondary', {
+  env: envSecondaryRegion
+});
 
 // Data Stack
 const dataStack = new EcsDataStack(app, 'workshop-data', {
   env: envMainRegion,
-  tableName: dynamodbTableName
+  tableName: dynamodbTableName,
+  replicationRegions: [envSecondaryRegion.region!]
 });
 
 // Backend Stacks
@@ -32,9 +40,20 @@ const mainBackendStack = new EcsBackendStack(app, 'workshop-backend-main', {
   tableName: dynamodbTableName,
   env: envMainRegion
 });
-
-// Routing Stack
-const routingStack = new EcsRoutingStack(app, 'workshop-routing', {
-  loadBalancer: mainBackendStack.apiService.loadBalancer,
-  env: envMainRegion,
+const secondaryBackendStack = new EcsBackendStack(app, 'workshop-backend-secondary', {
+  vpc: secondaryFoundationStack.vpc,
+  tableName: dynamodbTableName,
+  env: envSecondaryRegion
 });
+
+if (process.env.WORKSHOP_SECONDARY_ALB_ARN) {
+
+  // Routing Stack
+  const routingStack = new EcsRoutingStack(app, 'workshop-routing', {
+    loadBalancer: mainBackendStack.apiService.loadBalancer,
+    env: envMainRegion,
+    secondaryLoadBalancerArn: process.env.WORKSHOP_SECONDARY_ALB_ARN
+  });
+
+}
+
